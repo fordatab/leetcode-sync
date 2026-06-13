db6a6fe32f3978a59504633ec9fd63699df34d23
Eric <ugcire132@gmail.com>
2026-06-13 23:49:48 +0000

    resubmit(3405): count-the-number-of-special-characters-ii

--- problems/3405-count-the-number-of-special-characters-ii/solution.cpp
+++ problems/3405-count-the-number-of-special-characters-ii/solution.cpp
@@ -3,9 +3,9 @@ public:
     int numberOfSpecialChars(string word) {
         vector<pair<int, int>> map(26);
 
-        for (char& c : word) {
+        for (char c : word) {
             if ('a' <= c and c <= 'z') {
-                if (map[c-'a'].second > 0) {
+                if ((map[c-'a'].second > 0) or (map[c-'a'].first == -1)) {
                     map[c-'a'].first = -1;
                 } else {
                     map[c-'a'].first = 1; 
@@ -16,8 +16,10 @@ public:
             }
         }
         int ct = 0;
-        for (auto& p : map) {
-            if ((p.first == 1) and p.second) ct++;
+        for (auto p : map) {
+            if ((p.first == 1) and p.second) {
+                ct++;
+            }
         }
         return ct;    
     }

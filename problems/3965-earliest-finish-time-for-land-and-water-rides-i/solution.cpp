c991d930ce7d6f6271ab9620c101486349d4d7ef
Eric <ugcire132@gmail.com>
2026-06-14 00:15:29 +0000

    resubmit(3965): earliest-finish-time-for-land-and-water-rides-i

--- problems/3965-earliest-finish-time-for-land-and-water-rides-i/solution.cpp
+++ problems/3965-earliest-finish-time-for-land-and-water-rides-i/solution.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime,
-                           vector<int>& landDuration,
-                           vector<int>& waterStartTime,
-                           vector<int>& waterDuration) {
-        int n = landStartTime.size();
-        int m = waterStartTime.size();
-        int res = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int land = landStartTime[i] + landDuration[i];
-                int land_water =
-                    max(land, waterStartTime[j]) + waterDuration[j];
-                res = min(res, land_water);
-
-                int water = waterStartTime[j] + waterDuration[j];
-                int water_land = max(water, landStartTime[i]) + landDuration[i];
-                res = min(res, water_land);
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int out = INT_MAX;
+        for (int i = 0; i < landStartTime.size(); i++) {
+            for (int j = 0; j < waterStartTime.size(); j++) {
+                // int landEnd = landStartTime[i] + landDuration[i];
+                // int waterEnd = waterStartTime[j] + waterDuration[j];
+                // if (landEnd <= waterStartTime[j]) {
+                //     out = min(out, waterEnd);
+                // } 
+                // if (waterEnd <= landStartTime[i]) {
+                //     out = min(out, landEnd);
+                // }
+                if (landStartTime[i] + landDuration[i] >= waterStartTime[j]) {
+                    out = min(out, landStartTime[i] + landDuration[i] + waterDuration[j]); 
+                }
+                if (waterStartTime[j] + waterDuration[j] >= landStartTime[i]) {
+                    out = min(out, waterStartTime[j] + waterDuration[j] + landDuration[i]); 
+                }
+                if (waterStartTime[j] + waterDuration[j] < landStartTime[i]) {
+                    out = min(out, landStartTime[i] + landDuration[i]);
+                }
+                if (landStartTime[i] + landDuration[i] < waterStartTime[j]) {
+                    out = min(out, waterStartTime[j] + waterDuration[j]);
+                }
+                // int out = min(out, min(landStartTime[i] + landDuration[i]))
             }
         }
-        return res;
+        return out;
     }
 };

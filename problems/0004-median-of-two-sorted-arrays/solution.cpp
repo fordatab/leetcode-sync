9d1e306c4144cb9fe6335b7047753c61f2c76a65
Eric <ugcire132@gmail.com>
2026-06-14 06:29:44 +0000

    resubmit(0004): median-of-two-sorted-arrays

--- problems/0004-median-of-two-sorted-arrays/solution.cpp
+++ problems/0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() == 0) {
-            return nums2.size() % 2 ? nums2[nums2.size()/2] : (nums2[nums2.size()/2]+nums2[nums2.size()/2-1])/2.0;
-        }
-        if (nums2.size() == 0) {
-            return nums1.size() % 2 ? nums1[nums1.size()/2] : (nums1[nums1.size()/2]+nums1[nums1.size()/2-1])/2.0;
-        }
-        vector<int>& A = nums1;
-        vector<int>& B = nums2;
-        if (B.size() < A.size()) {
-            swap(A, B);
-        }
-        int total = nums1.size() + nums2.size();
-        int half = (total+1)/2;
-        int l = 0;
-        int r = nums1.size();
-        while (l <= r) {
-            int mid = (r - l)/2 + l;
-            int mid_2 = half - mid;
-
-            int a_l = (mid > 0) ? nums1[mid-1] : INT_MIN;
-            int a_r = (mid < nums1.size()) ? nums1[mid] : INT_MAX;
-            int b_l = (mid_2 > 0) ? nums2[mid_2-1] : INT_MIN;
-            int b_r = (mid_2 < nums2.size()) ? nums2[mid_2] : INT_MAX;
-            cout << a_l << " " << a_r << " " << b_l << " " << b_r << endl;
-            if (a_l <= b_r && b_l <= a_r) {
-                if (total % 2 != 0) { // odd
-                    return max(a_l, b_l);
-                } else {
-                    return (max(a_l, b_l) + min(a_r, b_r))/2.0;
-                }
+       
+        int i=0;
+        int j=0;
+        int n=nums1.size();
+        int m=nums2.size();
+        vector<int> merge(n+m);
+        int k=0;
+        while(i<n && j<m){
+            if(nums1[i]<=nums2[j]){
+                merge[k++]=(nums1[i++]);
             }
-            if (a_l > b_r) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+            else{
+                merge[k++]=(nums2[j++]);
             }
         }
-        return -1;
+        while(i<n){
+            merge[k++]=(nums1[i++]);
+        }
+        while(j<m){
+            merge[k++]=(nums2[j++]);
+        }
+        int low=0;
+        int high=m+n-1;
+        int mid=(low+high)/2;
+        double ans=0;
+        if(merge.size()%2==0){
+            ans=(merge[mid]+merge[mid+1])/2.0;
+        }
+        else{
+            ans=merge[mid];
+        }
+        return ans;
     }
 };

c5ecf7468435a705c2cf980acee19cca0b09bd4b
Eric <ugcire132@gmail.com>
2026-06-15 03:48:36 +0000

    resubmit(0018): 4sum

--- problems/0018-4sum/solution.cpp
+++ problems/0018-4sum/solution.cpp
@@ -1,34 +1,65 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> output;
-        for (int i = 0; i < n - 3; i++) {
-            for (int j = i + 1; j < n - 2; j++) {
-                long long newTarget = (long long)target - nums[i] - nums[j];
-                int low = j + 1, high = n - 1;
-                while (low < high) {
-                    if (nums[low] + nums[high] < newTarget) {
-                        low++;
-                    } else if (nums[low] + nums[high] > newTarget) {
-                        high--;
-                    } else {
-                        output.push_back(
-                            {nums[i], nums[j], nums[low], nums[high]});
-                        int tempIndex1 = low, tempIndex2 = high;
-                        while (low < high && nums[low] == nums[tempIndex1])
-                            low++;
-                        while (low < high && nums[high] == nums[tempIndex2])
-                            high--;
-                    }
-                }
-                while (j + 1 < n && nums[j] == nums[j + 1])
-                    j++;
-            }
-            while (i + 1 < n && nums[i] == nums[i + 1])
-                i++;
+        sort(begin(nums), end(nums));
+        return kSum(nums, target, 0, 4);
+    }
+
+    vector<vector<int>> kSum(vector<int>& nums, long long target, int start,
+                             int k) {
+        vector<vector<int>> res;
+
+        // If we have run out of numbers to add, return res.
+        if (start == nums.size()) {
+            return res;
         }
-        return output;
+
+        // There are k remaining values to add to the sum. The
+        // average of these values is at least target / k.
+        long long average_value = target / k;
+
+        // We cannot obtain a sum of target if the smallest value
+        // in nums is greater than target / k or if the largest
+        // value in nums is smaller than target / k.
+        if (nums[start] > average_value || average_value > nums.back()) {
+            return res;
+        }
+
+        if (k == 2) {
+            return twoSum(nums, target, start);
+        }
+
+        for (int i = start; i < nums.size(); ++i) {
+            if (i == start || nums[i - 1] != nums[i]) {
+                for (vector<int>& subset :
+                     kSum(nums, static_cast<long long>(target) - nums[i], i + 1,
+                          k - 1)) {
+                    res.push_back({nums[i]});
+                    res.back().insert(end(res.back()), begin(subset),
+                                      end(subset));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    vector<vector<int>> twoSum(vector<int>& nums, long long target, int start) {
+        vector<vector<int>> res;
+        int lo = start, hi = int(nums.size()) - 1;
+
+        while (lo < hi) {
+            int curr_sum = nums[lo] + nums[hi];
+            if (curr_sum < target || (lo > start && nums[lo] == nums[lo - 1])) {
+                ++lo;
+            } else if (curr_sum > target ||
+                       (hi < nums.size() - 1 && nums[hi] == nums[hi + 1])) {
+                --hi;
+            } else {
+                res.push_back({nums[lo++], nums[hi--]});
+            }
+        }
+
+        return res;
     }
 };
